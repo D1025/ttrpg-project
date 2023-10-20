59a95c770b9d5b25948b3b438bc1de97cb927781
Patryk Treszczotko <patryk5531@gmail.com>
2023-10-20 20:22:51 +0200

    feat: initial

---
+++ backend/src/test/java/com/ttrpg/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttrpg.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	// @Test
+	// void contextLoads() {
+	// }
+
+}
