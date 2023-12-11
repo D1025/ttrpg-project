a4f7568cd1e15495324e7a1455b586cad81b67b0
Donald <patryk5531@gmail.com>
2023-12-11 14:29:45 +0100

    fix

--- backend/src/main/java/com/ttrpg/project/configuration/ProjectProperties.java
+++ backend/src/main/java/com/ttrpg/project/configuration/ProjectProperties.java
@@ -1,5 +1,6 @@
 package com.ttrpg.project.configuration;
 
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +9,7 @@ import lombok.Getter;
 @Configuration
 @ConfigurationProperties(prefix = "project")
 @Getter
+@Setter
 public class ProjectProperties {
     private Integer expirationTime;
 }
