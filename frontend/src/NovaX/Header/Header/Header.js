bcce32ecc4c696f98e40fad3e2022bd20aa395e4
Xeno <adrian70000000@gmail.com>
2024-01-21 00:50:08 +0100

    poprawk

--- frontend/src/NovaX/Header/Header/Header.js
+++ frontend/src/NovaX/Header/Header/Header.js
@@ -1,12 +1,13 @@
 import './Header.css';
 
-const Header = ({children, design=1, ...rest}) =>
+const Header = ({children, design = 1, src = "", ...rest}) =>
 {
     let klasy = "";
     if(design > 0) klasy += "Header-D" + design;
 
     return (
-        <div {...rest} className={klasy}>
+        <div {...rest} className={klasy}
+             style={{backgroundImage: `linear-gradient(to right, var(--Motyw), transparent 190%), ${src}`, ...rest.style}}>
             {children}
         </div>
     );
