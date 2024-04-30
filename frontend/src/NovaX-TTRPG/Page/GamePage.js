6d4e5c490f720afe625339ae3412b9c1efd08e07
Xeno <adrian70000000@gmail.com>
2024-04-30 22:14:57 +0200

    Mniejsze poprawki.

--- frontend/src/NovaX-TTRPG/Page/GamePage.js
+++ frontend/src/NovaX-TTRPG/Page/GamePage.js
@@ -1,8 +1,13 @@
 import {useCallback, useEffect, useState} from "react";
 import {
-    storageLoad, setTittle, storageRemove,
+    storageLoad,
+    setTittle
 } from "../../NovaX";
-import {ModulChat, ModulHeader, useLogOut, WindowLogIn} from "../../NovaX-TTRPG";
+import {
+    ModulChat,
+    ModulHeader,
+    useLogOut
+} from "../../NovaX-TTRPG";
 
 const GamePage = () =>
 {
@@ -18,18 +23,10 @@ const GamePage = () =>
     // Wylogowywanie.
     const LogOut = useLogOut(userData, setIsLogIn, setUserData);
 
-    // Formularz Logowanie/Rejestracja.
-    const [showLogIn, setShowLogIn] = useState(false);
-    const toggleLogIn = () =>
-    {
-        setShowLogIn(prevShowLogin => !prevShowLogin);
-    };
-
     // Sprawdza logowanie i odświeża dynamiczne elementy po zmianie.
     useEffect(() =>
     {
         const loginData = storageLoad('loginData');
-        // Jeśli dane logowania istnieją.
         if(loginData)
         {
             setIsLogIn(true);
@@ -37,18 +34,18 @@ const GamePage = () =>
         }
         else
         {
+            setIsLogIn(false);
+            setUserData('');
             window.location.href = '/';
         }
-    }, [showLogIn === false, LogOut]);
+    }, [LogOut]);
 
     return (
         <>
             {/* Nagłłówek Strony. */}
-            <ModulHeader isLogIn={isLogIn} userData={userData} logOut={LogOut} navUser={false} navGame={true} logIn={toggleLogIn}/>
+            <ModulHeader isLogIn={isLogIn} userData={userData} logOut={LogOut} navUser={false} navGame={true}/>
 
             <ModulChat roomId={idParam} userId={userData.id}/>
-
-            {showLogIn && <WindowLogIn onClose={toggleLogIn}/>}
         </>
     );
 }
